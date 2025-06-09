ed7b75aa83b8b29f627d7eca1280cb382f98a517
Alex Borisov <borisovalexdev@gmail.com>
2025-06-09 15:32:12 +0300

    refactor: add accessibility attributes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -32,19 +32,19 @@ export const Home = () => {
 
     return (
         <main className={styles.main}>
-            <div className={styles.image_slider}>
+            <section className={styles.image_slider}>
                 <ImageSlider images={IMAGES} />
-            </div>
+            </section>
 
-            <div className={styles.welcome_text}>
+            <section className={styles.welcome_text}>
                 <span>Welcome to SneakersStore </span> the world of stylish and comfortable sneakers! We
                 are happy to welcome you to our website, where <span>every step</span> will become{' '}
                 <span>easy </span> and <span>confident</span>. Here you will find a carefully selected
                 collection of footwear that combines the latest trends, innovative technologies and the
                 highest quality.
-            </div>
+            </section>
 
-            <div className={styles.catalog}>
+            <section className={styles.catalog}>
                 {sneakers.length ? (
                     sneakers
                         .slice(firstContentIndex, lastContentIndex)
@@ -52,32 +52,34 @@ export const Home = () => {
                 ) : (
                     <p> {`Sorry, we don't have any sneakers :(`} </p>
                 )}
-            </div>
+            </section>
 
-            <div className={styles.pagination}>
+            <section className={styles.pagination}>
                 <p className={styles.text}>
                     <span>{page}</span> / {totalPages}
                 </p>
                 <div className={styles.flex}>
                     <div className={styles.page} onClick={prevPage}>
-                        <ArrowBigLeft />
+                        <ArrowBigLeft aria-label='Previous picture' />
                     </div>
 
                     <div className={styles.page} onClick={nextPage}>
-                        <ArrowBigRight />
+                        <ArrowBigRight aria-label='Next picture'/>
                     </div>
                 </div>
-            </div>
+            </section>
 
-            <div className={styles.find_us}>
+            <section className={styles.find_us} aria-label="Location and map">
                 <p>How do you find us?</p>
                 <iframe
                     title="map"
                     className={styles.map}
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.1037470604424!2d-73.97939322440051!3d40.75974283469214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c258fbfcb360d7%3A0x47415655d60b3a1a!2s650%205th%20Ave%2C%20New%20York%2C%20NY%2010019%2C%20USA!5e0!3m2!1sen!2sby!4v1727559117124!5m2!1sen!2sby"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2446.5519808883964!2d20.987456676413437!3d52.17883866143235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47193323a32b618f%3A0xd4247ada3a3f46cd!2sTa%C5%9Bmowa%204%2C%2002-677%20Warszawa%2C%20Poland!5e0!3m2!1sen!2sby!4v1749467038752!5m2!1sen!2sby"
                     loading="lazy"
+                    allowFullScreen
+                    referrerPolicy="no-referrer-when-downgrade"
                 ></iframe>
-            </div>
+            </section>
         </main>
     );
 };
