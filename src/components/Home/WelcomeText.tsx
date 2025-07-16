dba3a2e05b29acb6a210740b516de34e0b851bb7
Alex Borisov <borisovalexdev@gmail.com>
2025-07-16 15:59:56 +0300

    refactor: Transfer markup to a separate component

---
+++ src/components/Home/WelcomeText.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const WelcomeText = () => {
+    return (
+        <>
+            <span>Welcome to SneakersStore </span> the world of stylish and comfortable sneakers! We are
+            happy to welcome you to our website, where <span>every step</span> will become{' '}
+            <span>easy </span> and <span>confident</span>. Here you will find a carefully selected
+            collection of footwear that combines the latest trends, innovative technologies and the
+            highest quality.
+        </>
+    );
+};
